457ba29fdef95e8a5954c5ec6ae34677bf0e30cc
KU <ku@digitaldolphins.jp>
2014-06-25 11:40:20 +0900

    HttpWebRequestで測定。

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace LANSpeedChecker {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tsc = new System.Windows.Forms.ToolStripContainer();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.bCheck = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.tbURL = new System.Windows.Forms.ToolStripTextBox();
+            this.sc1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbLOG = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.lStat = new System.Windows.Forms.ToolStripLabel();
+            this.tsc.ContentPanel.SuspendLayout();
+            this.tsc.TopToolStripPanel.SuspendLayout();
+            this.tsc.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.sc1.Panel1.SuspendLayout();
+            this.sc1.Panel2.SuspendLayout();
+            this.sc1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tsc
+            // 
+            // 
+            // tsc.ContentPanel
+            // 
+            this.tsc.ContentPanel.Controls.Add(this.sc1);
+            this.tsc.ContentPanel.Size = new System.Drawing.Size(667, 429);
+            this.tsc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tsc.Location = new System.Drawing.Point(0, 0);
+            this.tsc.Name = "tsc";
+            this.tsc.Size = new System.Drawing.Size(667, 454);
+            this.tsc.TabIndex = 0;
+            // 
+            // tsc.TopToolStripPanel
+            // 
+            this.tsc.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bCheck,
+            this.toolStripSeparator1,
+            this.toolStripLabel1,
+            this.tbURL,
+            this.toolStripSeparator2,
+            this.lStat});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(667, 25);
+            this.toolStrip1.Stretch = true;
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // bCheck
+            // 
+            this.bCheck.Image = global::LANSpeedChecker.Properties.Resources.PlayHS;
+            this.bCheck.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.bCheck.Name = "bCheck";
+            this.bCheck.Size = new System.Drawing.Size(68, 22);
+            this.bCheck.Text = "&Check!";
+            this.bCheck.Click += new System.EventHandler(this.bCheck_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(32, 22);
+            this.toolStripLabel1.Text = "URL";
+            // 
+            // tbURL
+            // 
+            this.tbURL.Name = "tbURL";
+            this.tbURL.Size = new System.Drawing.Size(299, 25);
+            this.tbURL.Text = "https://news.google.co.jp/";
+            // 
+            // sc1
+            // 
+            this.sc1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sc1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.sc1.Location = new System.Drawing.Point(0, 0);
+            this.sc1.Name = "sc1";
+            this.sc1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // sc1.Panel1
+            // 
+            this.sc1.Panel1.AutoScroll = true;
+            this.sc1.Panel1.Controls.Add(this.pictureBox1);
+            this.sc1.Panel1.Controls.Add(this.label2);
+            // 
+            // sc1.Panel2
+            // 
+            this.sc1.Panel2.Controls.Add(this.tbLOG);
+            this.sc1.Panel2.Controls.Add(this.label1);
+            this.sc1.Size = new System.Drawing.Size(667, 429);
+            this.sc1.SplitterDistance = 249;
+            this.sc1.SplitterWidth = 6;
+            this.sc1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "生データ。ミリ秒 と レスポンスボディのバイト数：";
+            // 
+            // tbLOG
+            // 
+            this.tbLOG.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbLOG.Location = new System.Drawing.Point(0, 12);
+            this.tbLOG.Multiline = true;
+            this.tbLOG.Name = "tbLOG";
+            this.tbLOG.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbLOG.Size = new System.Drawing.Size(667, 162);
+            this.tbLOG.TabIndex = 1;
+            this.tbLOG.WordWrap = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "グラフ。縦100バイト×1ピクセル。横10ミリ秒×1ピクセル";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(299, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // lStat
+            // 
+            this.lStat.Name = "lStat";
+            this.lStat.Size = new System.Drawing.Size(20, 22);
+            this.lStat.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 454);
+            this.Controls.Add(this.tsc);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LAN Speed Checker";
+            this.tsc.ContentPanel.ResumeLayout(false);
+            this.tsc.TopToolStripPanel.ResumeLayout(false);
+            this.tsc.TopToolStripPanel.PerformLayout();
+            this.tsc.ResumeLayout(false);
+            this.tsc.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.sc1.Panel1.ResumeLayout(false);
+            this.sc1.Panel1.PerformLayout();
+            this.sc1.Panel2.ResumeLayout(false);
+            this.sc1.Panel2.PerformLayout();
+            this.sc1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer tsc;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton bCheck;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox tbURL;
+        private System.Windows.Forms.SplitContainer sc1;
+        private System.Windows.Forms.TextBox tbLOG;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripLabel lStat;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+
+namespace LANSpeedChecker {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        Stopwatch sw;
+
+        byte[] bin = new byte[65536];
+
+        class Ent {
+            public int r;
+            public long t;
+
+            public Ent(int r, long t) {
+                this.r = r;
+                this.t = t;
+            }
+        }
+
+        List<Ent> al = new List<Ent>(100);
+
+        private void bCheck_Click(object sender, EventArgs e) {
+            HttpWebRequest r1 = (HttpWebRequest)HttpWebRequest.Create(tbURL.Text);
+            r1.Method = "GET";
+            r1.UserAgent = "LANSpeedChecker/0.1";
+
+            al.Clear();
+
+            int tot = 0;
+
+            sw = new Stopwatch();
+            sw.Start();
+            using (HttpWebResponse r2 = (HttpWebResponse)r1.GetResponse()) {
+                Stream si = r2.GetResponseStream();
+                while (true) {
+                    int r = si.Read(bin, 0, bin.Length);
+                    if (r < 1) break;
+                    al.Add(new Ent(r, sw.ElapsedMilliseconds));
+                    tot += r;
+                }
+            }
+            sw.Stop();
+
+            StringWriter wr = new StringWriter();
+            foreach (Ent ent in al) {
+                wr.WriteLine(ent.t + " " + ent.r);
+            }
+            tbLOG.Text = wr.ToString();
+
+            {
+                int d = 10;
+                int cx = (int)(sw.ElapsedMilliseconds / d);
+
+                int sy = 100;
+
+                int cy = 200;
+                Bitmap pic = new Bitmap(cx + 1, cy + 1);
+                using (Graphics cv = Graphics.FromImage(pic)) {
+                    foreach (Ent ent in al) {
+                        int x0 = (int)(ent.t / d);
+                        int v = ent.r / sy;
+
+                        cv.DrawLine(Pens.BlueViolet, x0, cy - v, x0, cy);
+                    }
+                    cv.DrawLine(Pens.Green, 0, cy, cx + 1, cy);
+                }
+
+                pictureBox1.Image = pic;
+            }
+
+            lStat.Text = String.Format("{0:#,##0}ミリ秒、{1:#,##0}バイト", sw.ElapsedMilliseconds, tot);
+        }
+    }
+}
